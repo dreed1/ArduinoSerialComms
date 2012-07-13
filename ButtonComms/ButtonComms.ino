6415c83bcd4cb20032109e23576836ddfb0bc725
Dan Reed <dan@danreed.net>
2012-07-13 13:57:55 -0700

    a mite more documentation added

--- ButtonComms/ButtonComms.ino
+++ ButtonComms/ButtonComms.ino
@@ -1,6 +1,8 @@
+//Arduino sketch to utilize a physical button (set to pin #2)
+//to output serial commands to some PC (together with the bundled python serial sniffer)
+//and an RGB LED denoting activity
+//by Dan Reed 7/13/12
 
-// constants won't change. They're used here to 
-// set pin numbers:
 const int buttonPin = 2;     // the number of the pushbutton pin
 const int redPin =  6;      // the number of the red pin
 const int greenPin =  5;      // the number of the green pin
@@ -11,6 +13,7 @@ int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
   // initialize the LED pin as an output:
+  Serial.begin(9600);
   pinMode(redPin, OUTPUT);      
   pinMode(greenPin, OUTPUT);   
   pinMode(bluePin, OUTPUT);   
@@ -23,15 +26,16 @@ void loop(){
   buttonState = digitalRead(buttonPin);
 
   // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
   if (buttonState == HIGH) {     
     // turn LED on:    
+    Serial.print(1);
     digitalWrite(redPin, HIGH);  
     digitalWrite(greenPin, HIGH); 
     digitalWrite(bluePin, HIGH); 
   } 
   else {
     // turn LED off:
+    Serial.print(0);
     digitalWrite(redPin, LOW);  
     digitalWrite(greenPin, LOW); 
     digitalWrite(bluePin, LOW); 
--- pySerialSniffer/SerialSniffer.py
+++ pySerialSniffer/SerialSniffer.py
@@ -8,6 +8,7 @@ def loop():
 	print buttonPressed
 
 
+#this is pretty fragile, set it to whatever comms port your 'duino is hooked up to until I find a better solution
 ser = serial.Serial('/dev/tty.usbmodem1421', 9600, timeout=1)
 while(True):
 	loop()
