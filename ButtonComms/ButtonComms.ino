319207109bee8415c5da24be9c8c4e7bb8028532
Dan Reed <dan@danreed.net>
2012-07-13 14:26:30 -0700

    serial event now registers on button release, cleans up some bugginess

--- ButtonComms/ButtonComms.ino
+++ ButtonComms/ButtonComms.ino
@@ -1,13 +1,11 @@
-//Arduino sketch to utilize a physical button (set to pin #2)
-//to output serial commands to some PC (together with the bundled python serial sniffer)
-//and an RGB LED denoting activity
-//by Dan Reed 7/13/12
 
+// constants won't change. They're used here to 
+// set pin numbers:
 const int buttonPin = 2;     // the number of the pushbutton pin
 const int redPin =  6;      // the number of the red pin
 const int greenPin =  5;      // the number of the green pin
 const int bluePin =  3;      // the number of the blue pin
-
+boolean buttonPressed;
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
@@ -19,6 +17,7 @@ void setup() {
   pinMode(bluePin, OUTPUT);   
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);     
+  buttonPressed = false;
 }
 
 void loop(){
@@ -26,18 +25,29 @@ void loop(){
   buttonState = digitalRead(buttonPin);
 
   // check if the pushbutton is pressed.
-  if (buttonState == HIGH) {     
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {  
+    if(!buttonPressed){
+      buttonPressed = true;
+    }   
     // turn LED on:    
-    Serial.print(1);
-    digitalWrite(redPin, HIGH);  
-    digitalWrite(greenPin, HIGH); 
-    digitalWrite(bluePin, HIGH); 
+    Serial.print(0);
+    analogWrite(redPin, 255);  
+    analogWrite(greenPin, 12); 
+    analogWrite(bluePin, 15); 
+    
   } 
   else {
+    if(buttonPressed){
+      buttonPressed = false;
+     Serial.print(1); 
+    }
+    else{
+      Serial.print(0);  
+    }
     // turn LED off:
-    Serial.print(0);
-    digitalWrite(redPin, LOW);  
-    digitalWrite(greenPin, LOW); 
-    digitalWrite(bluePin, LOW); 
+    analogWrite(redPin, 0);  
+    analogWrite(greenPin, 0); 
+    analogWrite(bluePin, 0); 
   }
 }
--- pySerialSniffer/SerialSniffer.py
+++ pySerialSniffer/SerialSniffer.py
@@ -2,11 +2,20 @@
 #by Dan Reed 7/13/12
 
 import serial
+from time import sleep
 
 def loop():
 	buttonPressed = ser.read()
-	print buttonPressed
+	#uncomment for a debug stream
+	#print buttonPressed
+	if(buttonPressed=='1'):
+		print 'About to do some magic, stand back... this could be dangerous.'
+		doSomeMagic()
 
+def doSomeMagic():
+	print 'beginning magic'
+	sleep(5)
+	print 'done with the magic'
 
 #this is pretty fragile, set it to whatever comms port your 'duino is hooked up to until I find a better solution
 ser = serial.Serial('/dev/tty.usbmodem1421', 9600, timeout=1)
